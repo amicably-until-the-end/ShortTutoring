7c6fa319b30127ed8793971ff769ad13eb16de42
haechan29 <haechan.im@gmail.com>
2023-07-05 05:06:29 +0900

    Add Hilt dependency

---
+++ app/src/main/java/org/softwaremaestro/shorttutoring/ExampleApplication.kt
@@ -0,0 +1,9 @@
+package org.softwaremaestro.shorttutoring
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ExampleApplication : Application() {
+
+}
