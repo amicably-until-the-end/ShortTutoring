cde8249b1b4123fa9e6136b5fafb249e64c60b84
haechan29 <haechan.im@gmail.com>
2023-07-05 10:58:59 +0900

    Add Coroutine dependency to data, presenter module

--- domain/src/main/java/org/softwaremaestro/domain/usecase/SendRequestUseCase.kt
+++ domain/src/main/java/org/softwaremaestro/domain/usecase/SendRequestUseCase.kt
@@ -1,4 +1,20 @@
 package org.softwaremaestro.domain.usecase
 
-class SendRequestUseCase() {
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import org.softwaremaestro.domain.model.Request
+import org.softwaremaestro.domain.repository.RequestRepository
+import kotlin.coroutines.CoroutineContext
+
+class SendRequestUseCase(val requestRepository: RequestRepository) {
+
+    operator fun invoke(
+        scope: CoroutineScope,
+        request: Request
+    ) {
+        scope.launch(Dispatchers.IO) {
+            requestRepository.sendRequest(request)
+        }
+    }
 }
