933e183a24c8de4f24fdb8b1a0dcca1f1e6fe41b
haechan29 <haechan.im@gmail.com>
2023-07-05 13:32:08 +0900

    Add ViewModel, LiveData dependency

--- presenter/src/main/java/org/softwaremaestro/presenter/StudentHomeActivity.kt
+++ presenter/src/main/java/org/softwaremaestro/presenter/StudentHomeActivity.kt
@@ -2,10 +2,12 @@ package org.softwaremaestro.presenter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
 
 class StudentHomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_home)
+        
     }
 }
