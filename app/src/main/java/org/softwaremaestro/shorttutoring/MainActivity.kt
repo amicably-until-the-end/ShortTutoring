8ee4e6f017bac1bffbffe100826a9f8e605c135d
haechan29 <haechan.im@gmail.com>
2023-07-06 16:07:08 +0900

    rebase main

--- app/src/main/java/org/softwaremaestro/shorttutoring/MainActivity.kt
+++ app/src/main/java/org/softwaremaestro/shorttutoring/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import dagger.hilt.android.AndroidEntryPoint
 import org.softwaremaestro.presenter.login.LoginActivity
-import org.softwaremaestro.presenter.question_upload.QuestionUploadActivity
 
 
 @AndroidEntryPoint
@@ -18,18 +17,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.btn_next).setOnClickListener {
-            val intent = Intent(this, QuestionUploadActivity::class.java)
-            startActivity(intent)
-        }
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
 
-        val toolbar: Toolbar = findViewById<Toolbar>(R.id.toolbar_main).also {
-            setSupportActionBar(it)
-        }
+        // 액션바 설정
+        val toolbar: Toolbar = findViewById<Toolbar>(R.id.toolbar_main)
+        if (toolbar != null)
+            setSupportActionBar(toolbar)
 
-        val actionBar = supportActionBar?.apply {
-            setDisplayHomeAsUpEnabled(true)
-        }
+        // 뒤로가기 버튼
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
