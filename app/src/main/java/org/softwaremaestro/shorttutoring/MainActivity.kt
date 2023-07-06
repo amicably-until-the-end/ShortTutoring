03ee1b109aae7e911011e92dd5809e4db650226e
haechan29 <haechan.im@gmail.com>
2023-07-06 13:51:57 +0900

    rebase main to login

--- app/src/main/java/org/softwaremaestro/shorttutoring/MainActivity.kt
+++ app/src/main/java/org/softwaremaestro/shorttutoring/MainActivity.kt
@@ -1,12 +1,16 @@
 package org.softwaremaestro.shorttutoring
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import dagger.hilt.android.AndroidEntryPoint
 import org.softwaremaestro.presenter.login.LoginActivity
 import org.softwaremaestro.presenter.question_upload.QuestionUploadActivity
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -17,6 +21,25 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.btn_next).setOnClickListener {
             val intent = Intent(this, QuestionUploadActivity::class.java)
             startActivity(intent)
+        }
+
+        val toolbar: Toolbar = findViewById<Toolbar>(R.id.toolbar_main).also {
+            setSupportActionBar(it)
+        }
+
+        val actionBar = supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
