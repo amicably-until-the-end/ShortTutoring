ec0d4216a342073741811cf481c3460d4bcf69ee
haechan29 <haechan.im@gmail.com>
2023-07-08 10:05:46 +0900

    Complete register_teacher_info_fragment

--- presenter/src/main/java/org/softwaremaestro/presenter/login/LoginActivity.kt
+++ presenter/src/main/java/org/softwaremaestro/presenter/login/LoginActivity.kt
@@ -7,7 +7,6 @@ import org.softwaremaestro.presenter.databinding.ActivityLoginBinding
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
