a723bf850098c185eefe980b02bbbf94fd13d4c0
seongyunlee <private.seongyun@gmail.com>
2023-07-11 17:14:13 +0900

    (question) make teacher select view and recycler adapter without remote data settings

--- presenter/src/main/java/org/softwaremaestro/presenter/question_upload/QuestionFormFragment.kt
+++ presenter/src/main/java/org/softwaremaestro/presenter/question_upload/QuestionFormFragment.kt
@@ -1,26 +1,17 @@
 package org.softwaremaestro.presenter.question_upload
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.util.Base64
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import dagger.hilt.android.AndroidEntryPoint
-import org.softwaremaestro.domain.question_upload.entity.QuestionUploadVO
 import org.softwaremaestro.presenter.R
 import org.softwaremaestro.presenter.databinding.FragmentQuestionFormBinding
-import java.io.ByteArrayOutputStream
-import java.io.File
 
 
 @AndroidEntryPoint
@@ -30,15 +21,8 @@ class QuestionFormFragment : Fragment() {
 
     private val viewModel: QuestionUploadViewModel by viewModels()
 
-    private var mathSubjects = HashMap<String, HashMap<String, HashMap<String, Int>>>()
+    private var mathSubjects = HashMap<String, HashMap<String, List<Pair<Int, String>>>>()
 
-    var pictureBitmap: Bitmap? = null
-
-    var schoolSelected: String = "고등학교"
-    var subjectSelected: String? = null
-    var chapterCodeSelected: Int? = null
-    var chapterSelected: String? = null
-    var difficultySelected: String = "normal"
 
     private fun parseMathSubjectJson() {
         val assestManager = resources.assets
@@ -46,11 +30,7 @@ class QuestionFormFragment : Fragment() {
             val inputStream = assestManager.open("mathSubjectLabels.json")
             val reader = inputStream.bufferedReader()
             val gson = Gson()
-            // Define the type of the outermost structure
-            val type =
-                object : TypeToken<HashMap<String, HashMap<String, HashMap<String, Int>>>>() {}.type
-            // Parse the JSON string and populate mathSubjects
-            mathSubjects = gson.fromJson(reader, type)
+            mathSubjects = gson.fromJson(reader, mathSubjects.javaClass)
         } catch (e: Exception) {
             return
         }
@@ -68,140 +48,68 @@ class QuestionFormFragment : Fragment() {
     ): View? {
 
         binding = FragmentQuestionFormBinding.inflate(inflater, container, false)
-        setButtons()
+
+        /*        binding.btnSubmit.setOnClickListener {
+                    viewModel.uploadQuestion(
+                        QuestionUploadVO(binding.etDetail.text.toString(),binding)
+
+                }*/
+        observe()
+
+        setSpinners()
         setRadioGroups()
-        setSubjectSpinner()
-        setObserver()
-        setSpinnerListener()
-        Log.d("bundle", arguments.toString())
-        setPicture(arguments?.getString("fileName")!!)
         return binding.root
-    }
-
-    private fun setPicture(fileName: String) {
-        val file = File(requireContext().filesDir, fileName)
-        if (file.exists()) {
-            pictureBitmap = BitmapFactory.decodeFile(file.absolutePath)
-            binding.ivPhoto.setImageBitmap(pictureBitmap)
-            Log.d("mymy", "fileexist")
-        }
-
-    }
-
-    private fun setObserver() {
-        viewModel.uploadResult.observe(viewLifecycleOwner) {
-            Toast.makeText(context, viewModel.uploadResult.value, Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    fun bitmapToBase64(bitmap: Bitmap): String {
-        val byteArrayOutputStream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
-        val byteArray = byteArrayOutputStream.toByteArray()
-        return Base64.encodeToString(byteArray, Base64.DEFAULT)
-    }
-
-    private fun setButtons() {
-        binding.btnSubmit.setOnClickListener {
-            viewModel.uploadQuestion(
-                QuestionUploadVO(
-                    "testID",
-                    binding.etDetail.text.toString(),
-                    bitmapToBase64(pictureBitmap!!),
-                    schoolSelected,
-                    subjectSelected ?: "",
-                    chapterSelected ?: "",
-                    difficultySelected
-                )
-            )
-        }
-    }
-
-    private fun setSubjectSpinner() {
-
-
-        val adapter = ArrayAdapter<String>(
-            requireContext(),
-            android.R.layout.simple_spinner_item,
-            mathSubjects[schoolSelected]?.keys?.toList()!!
-        )
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.spinnerSubject.adapter = adapter
-    }
-
-    private fun setChapterSpinner() {
-
-        val adapter = ArrayAdapter<String>(
-            requireContext(),
-            android.R.layout.simple_spinner_item,
-            mathSubjects[schoolSelected]?.get(subjectSelected)?.keys?.toList()!!
-        )
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        binding.spinnerChapter.adapter = adapter
     }
 
     private fun setRadioGroups() {
         binding.rgSchool.setOnCheckedChangeListener { _, checkId ->
             when (checkId) {
-                R.id.rb_middleschool -> schoolSelected = "중학교"
+                R.id.rb_middleschool -> {
+                    viewModel.schoolSelected_.value = "중학교"
+                }
 
-
-                R.id.rb_highschool -> schoolSelected = "고등학교"
-
+                R.id.rb_highschool -> {
+                    viewModel.schoolSelected_.value = "고등학교"
+                }
             }
-            setSubjectSpinner()
         }
         binding.rgDifficulty.setOnCheckedChangeListener { _, checkId ->
             when (checkId) {
-                R.id.rb_easy -> difficultySelected = "easy"
-
-                R.id.rb_normal -> difficultySelected = "normal"
-
-
-                R.id.rb_hard -> difficultySelected = "hard"
-
+                R.id.rb_easy -> viewModel.schoolSelected_.value = "easy"
+                R.id.rb_normal -> viewModel.schoolSelected_.value = "normal"
+                R.id.rb_hard -> viewModel.schoolSelected_.value = "hard"
             }
         }
     }
 
-
-    private fun setSpinnerListener() {
-        binding.spinnerSubject.onItemSelectedListener =
-            object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(
-                    parent: AdapterView<*>?,
-                    view: View?,
-                    pos: Int,
-                    id: Long
-                ) {
-                    val selectedItem = parent?.getItemAtPosition(pos).toString()!!
-                    Log.d("mymy", selectedItem)
-                    subjectSelected = selectedItem
-                    setChapterSpinner()
-                }
-
-                override fun onNothingSelected(p0: AdapterView<*>?) {
-                    return
-                }
-
-            }
-        binding.spinnerChapter.onItemSelectedListener =
-            object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(
-                    parent: AdapterView<*>?,
-                    view: View?,
-                    pos: Int,
-                    id: Long
-                ) {
-                    val selectedItem = parent?.getItemAtPosition(pos).toString()!!
-                    chapterSelected = selectedItem
-                }
-
-                override fun onNothingSelected(p0: AdapterView<*>?) {
-                    return
-                }
-
-            }
+    private fun setSpinners() {
+        val adapter = ArrayAdapter<String>(
+            requireContext(),
+            android.R.layout.simple_spinner_item,
+            mathSubjects.keys.toList()
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerChapter.adapter = adapter
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //observe()
+    }
+
+    private fun observe() {
+        /*viewModel.resultString.observe(viewLifecycleOwner, Observer {
+            Log.d("mymym", viewModel.resultString.value!!);
+        })*/
+        viewModel.schoolSelected.observe(viewLifecycleOwner, Observer {
+            val adapter = ArrayAdapter<String>(
+                requireContext(),
+                android.R.layout.simple_spinner_item,
+                mathSubjects[viewModel.schoolSelected.value]?.keys?.toList()!!
+            )
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spinnerChapter.adapter = adapter
+        })
+    }
 }
