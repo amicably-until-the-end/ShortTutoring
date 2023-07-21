dbf86e9f6a514bbff2efe37899b7ac3eedd2689b
haechan29 <haechan.im@gmail.com>
2023-07-21 13:48:46 +0900

    improve student register layout

--- presenter/src/main/java/org/softwaremaestro/presenter/login/RegisterStudentInfoFragment.kt
+++ presenter/src/main/java/org/softwaremaestro/presenter/login/RegisterStudentInfoFragment.kt
@@ -1,27 +1,40 @@
 package org.softwaremaestro.presenter.login
 
-import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import dagger.hilt.android.AndroidEntryPoint
 import org.softwaremaestro.presenter.R
 import org.softwaremaestro.presenter.databinding.FragmentRegisterStudentInfoBinding
-import org.softwaremaestro.presenter.student_home.StudentHomeActivity
 
 @AndroidEntryPoint
 class RegisterStudentInfoFragment : Fragment() {
 
     private lateinit var binding: FragmentRegisterStudentInfoBinding
+    private var selectedSchool: Int = 0
+    private var selectedGrade: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentRegisterStudentInfoBinding.inflate(inflater, container, false)
+        binding = FragmentRegisterStudentInfoBinding.inflate(layoutInflater)
+
+        binding.rgSchoolLevel.setOnCheckedChangeListener { _, pos ->
+            selectedSchool = pos
+        }
+
+        binding.rgGrade.setOnCheckedChangeListener { _, pos ->
+            selectedGrade = pos
+        }
+
+        binding.btnNext.setOnClickListener {
+            Navigation.findNavController(requireView())
+                .navigate(R.id.action_registerStudentInfoFragment_to_logoFragment)
+        }
 
         return binding.root
     }
