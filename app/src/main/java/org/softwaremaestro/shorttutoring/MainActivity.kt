24687432bdc445de313f1b3faf6a8c5a5d67d5e4
seongyunlee <private.seongyun@gmail.com>
2023-07-24 17:27:49 +0900

    (feature) base setting for agora voice sdk okay

--- app/src/main/java/org/softwaremaestro/shorttutoring/MainActivity.kt
+++ app/src/main/java/org/softwaremaestro/shorttutoring/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import dagger.hilt.android.AndroidEntryPoint
+import org.softwaremaestro.presenter.classroom.ClassroomActivity
 import org.softwaremaestro.presenter.login.LoginActivity
 
 
@@ -15,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intent = Intent(this, LoginActivity::class.java)
+        val intent = Intent(this, ClassroomActivity::class.java)
         startActivity(intent)
 
         // 액션바 설정
