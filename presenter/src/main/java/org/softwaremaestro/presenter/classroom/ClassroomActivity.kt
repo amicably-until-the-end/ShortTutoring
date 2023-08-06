4d1d68ec4e4817aa8f7b434009205dd131b27187
haechan29 <haechan.im@gmail.com>
2023-08-07 08:40:42 +0900

    change layout of classroom

--- presenter/src/main/java/org/softwaremaestro/presenter/classroom/ClassroomActivity.kt
+++ presenter/src/main/java/org/softwaremaestro/presenter/classroom/ClassroomActivity.kt
@@ -1,9 +1,7 @@
 package org.softwaremaestro.presenter.classroom
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
-import org.softwaremaestro.presenter.R
+import androidx.appcompat.app.AppCompatActivity
 import org.softwaremaestro.presenter.databinding.ActivityClassroomBinding
 
 class ClassroomActivity : AppCompatActivity() {
@@ -14,6 +12,5 @@ class ClassroomActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityClassroomBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        
     }
 }
