550c2603c92e78be740657d551fa1fc99f59d4a9
haechan29 <haechan.im@gmail.com>
2023-08-07 07:43:04 +0900

    set bottom nav bar to teacher home

--- presenter/src/main/java/org/softwaremaestro/presenter/student_home/StudentMyPageFragment.kt
+++ presenter/src/main/java/org/softwaremaestro/presenter/student_home/StudentMyPageFragment.kt
@@ -1,10 +1,10 @@
 package org.softwaremaestro.presenter.student_home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import org.softwaremaestro.presenter.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SettingFragment.newInstance] factory method to
+ * Use the [StudentMyPageFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SettingFragment : Fragment() {
+class StudentMyPageFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class SettingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        return inflater.inflate(R.layout.fragment_student_my_page, container, false)
     }
 
     companion object {
@@ -50,7 +50,7 @@ class SettingFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SettingFragment().apply {
+            StudentMyPageFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- presenter/src/main/java/org/softwaremaestro/presenter/teacher_home/TeacherHomeActivity.kt
+++ presenter/src/main/java/org/softwaremaestro/presenter/teacher_home/TeacherHomeActivity.kt
@@ -1,24 +1,35 @@
 package org.softwaremaestro.presenter.teacher_home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.softwaremaestro.presenter.R
 import org.softwaremaestro.presenter.databinding.ActivityTeacherHomeBinding
-import org.softwaremaestro.presenter.databinding.FragmentTeacherHomeBinding
 
 @AndroidEntryPoint
 class TeacherHomeActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityTeacherHomeBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTeacherHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setUpBottomNavigationBar()
     }
 
-
+    /**
+     * Bottom Navigation Bar 에 Navigation Component 적용
+     */
+    fun setUpBottomNavigationBar() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNavView.setupWithNavController(navController)
+    }
 }
