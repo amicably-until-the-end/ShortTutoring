16b9d742eabb619b6e11e87fdcaca10d7a0dcde0
haechan29 <haechan.im@gmail.com>
2023-08-08 08:32:54 +0900

    change layout of teacher my page

--- presenter/src/main/java/org/softwaremaestro/presenter/teacher_my_page/TeacherMyPageFragment.kt
+++ presenter/src/main/java/org/softwaremaestro/presenter/teacher_my_page/TeacherMyPageFragment.kt
@@ -5,11 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import org.softwaremaestro.presenter.databinding.FragmentTeacherMyPageBinding
+import org.softwaremaestro.presenter.teacher_home.ReviewAdapter
+
+private const val teacherName = "유정연선생님"
+private const val teacherSchool = "서울대학교 수의예과"
+private const val rating = 4.8895f
+private const val numOfFollower = 122
+private const val numOfClip = 15
 
 class TeacherMyPageFragment : Fragment() {
 
     private lateinit var binding: FragmentTeacherMyPageBinding
+
+    private val reviewsViewModel: ReviewsViewModel by viewModels()
+
+    private lateinit var reviewAdapter: ReviewAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,6 +35,39 @@ class TeacherMyPageFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setExampleText()
 
+        initReviewRecyclerView()
+    }
+
+    private fun setExampleText() {
+        with(binding) {
+            tvTeacherName.text = teacherName
+            tvTeacherSchool.text = teacherSchool
+            tvRating.text = "%.2f".format(rating)
+            tvNumOfFollower.text = numOfFollower.toString()
+            tvNumOfClip.text = numOfClip.toString()
+        }
+    }
+
+    private fun initReviewRecyclerView() {
+
+        reviewAdapter = ReviewAdapter()
+
+        binding.rvReview.apply {
+            adapter = reviewAdapter
+            layoutManager =
+                LinearLayoutManager(requireActivity(), LinearLayoutManager.HORIZONTAL, false)
+        }
+
+        reviewsViewModel.reviews.observe(requireActivity()) {
+            binding.containerReviewEmpty.visibility =
+                if (it.isEmpty()) View.VISIBLE else View.INVISIBLE
+
+            reviewAdapter.setItem(it)
+            binding.tvNumOfReview.text = it.size.toString()
+        }
+
+        reviewsViewModel.getReviews()
     }
 }
