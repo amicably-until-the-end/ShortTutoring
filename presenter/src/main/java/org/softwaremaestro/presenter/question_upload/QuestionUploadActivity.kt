0cbda719851eac993d37c6dfe53928e252e83930
seongyunlee <private.seongyun@gmail.com>
2023-09-17 14:17:35 +0900

    카메라 권한 로직 수정

--- presenter/src/main/java/org/softwaremaestro/presenter/question_upload/QuestionUploadActivity.kt
+++ presenter/src/main/java/org/softwaremaestro/presenter/question_upload/QuestionUploadActivity.kt
@@ -1,8 +1,22 @@
 package org.softwaremaestro.presenter.question_upload
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
+import org.softwaremaestro.presenter.R
 import org.softwaremaestro.presenter.databinding.ActivityQuestionUploadBinding
 
 @AndroidEntryPoint
@@ -10,9 +24,49 @@ class QuestionUploadActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityQuestionUploadBinding
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityQuestionUploadBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+
+        getPermission()
     }
+
+    private fun getPermission() {
+        var permissionList = arrayOf<String>()
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            permissionList += Manifest.permission.CAMERA
+        }
+        if (permissionList.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionList, 1001)
+        } else {
+            setContentView(binding.root)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1001 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    setContentView(binding.root)
+                } else {
+                    Toast.makeText(this, "권한을 허용해주세요", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+        }
+    }
+
+
 }
