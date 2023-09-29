023c40b43a5d9f3a415570a83e58b13fe68bad75
haechan29 <haechan.im@gmail.com>
2023-09-29 14:49:14 +0900

    fix bug (delete some import im ChatModule)

--- data/src/main/java/org/softwaremaestro/data/chat/ChatModule.kt
+++ data/src/main/java/org/softwaremaestro/data/chat/ChatModule.kt
@@ -8,14 +8,9 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import org.softwaremaestro.data.chat.database.ChatDatabase
 import org.softwaremaestro.data.chat.remote.ChatApi
-import org.softwaremaestro.data.classroom.remote.ClassRoomApi
 import org.softwaremaestro.data.common.module.NetworkModule
-import org.softwaremaestro.data.my_profile_get.MyProfileGetRepositoryImpl
-import org.softwaremaestro.data.my_profile_get.remote.MyProfileGetApi
 import org.softwaremaestro.data.question_get.remote.QuestionGetApi
 import org.softwaremaestro.domain.chat.ChatRepository
-import org.softwaremaestro.domain.classroom.ClassRoomRepository
-import org.softwaremaestro.domain.my_profile_get.MyProfileGetRepository
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
