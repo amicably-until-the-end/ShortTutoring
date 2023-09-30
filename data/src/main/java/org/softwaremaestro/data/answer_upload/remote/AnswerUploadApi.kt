4a12ea352b10980c24858847632ef83bfa9f22ba
seongyunlee <private.seongyun@gmail.com>
2023-09-30 13:10:39 +0900

    fix dependencies error , @get->@post

--- data/src/main/java/org/softwaremaestro/data/answer_upload/remote/AnswerUploadApi.kt
+++ data/src/main/java/org/softwaremaestro/data/answer_upload/remote/AnswerUploadApi.kt
@@ -7,6 +7,7 @@ import org.softwaremaestro.data.common.utils.WrappedResponse
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface AnswerUploadApi {
@@ -15,7 +16,7 @@ interface AnswerUploadApi {
         @Path("questionId") requestId: String
     ): Response<WrappedResponse<AnswerUploadResultDto>>
 
-    @GET("/tutoring/appoint/{questionId}")
+    @POST("/tutoring/appoint/{questionId}")
     suspend fun pickStudent(
         @Path("questionId") questionId: String, @Body body: StudentPickReqDto
     ): Response<WrappedResponse<StudentPickResultDto>>
