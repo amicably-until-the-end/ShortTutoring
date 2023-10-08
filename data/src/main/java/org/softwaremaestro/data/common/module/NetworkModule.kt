c3c20b1b2476505879379063523dd57396775e19
seongyunlee <private.seongyun@gmail.com>
2023-10-08 12:42:06 +0900

    학생 채팅방에서 아무 offer 없는 일반 질문도 보이게

--- data/src/main/java/org/softwaremaestro/data/common/module/NetworkModule.kt
+++ data/src/main/java/org/softwaremaestro/data/common/module/NetworkModule.kt
@@ -21,7 +21,7 @@ object NetworkModule {
     fun provideRetrofit(okHttp: OkHttpClient): Retrofit {
         return Retrofit.Builder().apply {
             addConverterFactory(GsonConverterFactory.create())
-            baseUrl("http://10.0.2.2:3000")
+            baseUrl("http://shorttutoring-493721324.ap-northeast-2.elb.amazonaws.com/")
             client(okHttp)
         }.build()
     }
