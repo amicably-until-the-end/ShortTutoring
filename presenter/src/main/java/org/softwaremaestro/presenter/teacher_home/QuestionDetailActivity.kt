834120f133690f8f13ce9be4abc714d12c873c5f
haechan29 <haechan.im@gmail.com>
2023-10-11 08:56:54 +0900

    이미 신청한 수업은 수업 신청하기 버튼을 비활성화함

--- presenter/src/main/java/org/softwaremaestro/presenter/teacher_home/QuestionDetailActivity.kt
+++ presenter/src/main/java/org/softwaremaestro/presenter/teacher_home/QuestionDetailActivity.kt
@@ -1,13 +1,20 @@
 package org.softwaremaestro.presenter.teacher_home
 
-import android.content.RestrictionsManager.RESULT_ERROR
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import org.softwaremaestro.domain.answer_upload.entity.AnswerUploadVO
+import org.softwaremaestro.presenter.R
 import org.softwaremaestro.presenter.databinding.ActivityQuestionDetailBinding
+import org.softwaremaestro.presenter.teacher_home.TeacherHomeFragment.Companion.DESCRIPTION
+import org.softwaremaestro.presenter.teacher_home.TeacherHomeFragment.Companion.HOPE_TIME
+import org.softwaremaestro.presenter.teacher_home.TeacherHomeFragment.Companion.IMAGE
+import org.softwaremaestro.presenter.teacher_home.TeacherHomeFragment.Companion.OFFERED_ALREADY
+import org.softwaremaestro.presenter.teacher_home.TeacherHomeFragment.Companion.QUESTION_ID
+import org.softwaremaestro.presenter.teacher_home.TeacherHomeFragment.Companion.SUBJECT
 import org.softwaremaestro.presenter.teacher_home.adapter.QuestionDetailImagesAdapter
 import org.softwaremaestro.presenter.teacher_home.viewmodel.AnswerViewModel
 
@@ -18,11 +25,12 @@ class QuestionDetailActivity : AppCompatActivity() {
 
     private val viewModel: AnswerViewModel by viewModels()
 
-    var images: ArrayList<String>? = null
-    var subject: String? = null
-    var description: String? = null
-    var questionId: String? = null
-    var hopeTime: String? = null
+    private var images: ArrayList<String>? = null
+    private var subject: String? = null
+    private var description: String? = null
+    private var questionId: String? = null
+    private var hopeTime: String? = null
+    private var offeredAlready = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,8 +47,23 @@ class QuestionDetailActivity : AppCompatActivity() {
     }
 
     private fun setOfferButton() {
-        binding.btnOffer.setOnClickListener {
-            viewModel.uploadAnswer(AnswerUploadVO(questionId!!))
+        if (offeredAlready) {
+            binding.btnOffer.apply {
+                setBackgroundResource(R.drawable.bg_radius_5_grey)
+                setTextColor(resources.getColor(R.color.sub_text_grey, null))
+                setOnClickListener {
+                    Toast.makeText(this@QuestionDetailActivity, "이미 신청한 수업입니다", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+        } else {
+            binding.btnOffer.apply {
+                setBackgroundResource(R.drawable.bg_radius_5_grad_blue)
+                setTextColor(resources.getColor(R.color.white, null))
+                setOnClickListener {
+                    viewModel.uploadAnswer(AnswerUploadVO(questionId!!))
+                }
+            }
         }
     }
 
@@ -62,6 +85,7 @@ class QuestionDetailActivity : AppCompatActivity() {
         description = intent.getStringExtra(DESCRIPTION)
         questionId = intent.getStringExtra(QUESTION_ID)
         hopeTime = intent.getStringExtra(HOPE_TIME)
+        offeredAlready = intent.getBooleanExtra(OFFERED_ALREADY, false)
     }
 
     private fun setObserver() {
@@ -71,12 +95,10 @@ class QuestionDetailActivity : AppCompatActivity() {
                 intent.putExtra(OFFER_RESULT, OFFER_SUCCESS)
                 intent.putExtra(CHAT_ID, it.chatRoomId)
                 setResult(RESULT_OK, intent)
+                finish()
             } else {
-                val intent = intent
-                intent.putExtra(OFFER_RESULT, OFFER_FAIL)
-                setResult(RESULT_ERROR, intent)
+                Toast.makeText(this, "이미 신청한 수업입니다", Toast.LENGTH_SHORT).show()
             }
-            finish()
         }
     }
 
