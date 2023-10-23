cdb07a87ec050d6c9c9607cc8e088963e5c0b969
seongyunlee <private.seongyun@gmail.com>
2023-10-23 16:04:06 +0900

    fix / async. issue on get chat messages

--- data/src/main/java/org/softwaremaestro/data/chat/database/ChatDatabase.kt
+++ data/src/main/java/org/softwaremaestro/data/chat/database/ChatDatabase.kt
@@ -16,7 +16,7 @@ import org.softwaremaestro.data.chat.entity.MessageEntity
 
 @Database(
     entities = [ChatRoomEntity::class, MessageEntity::class],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 @TypeConverters(Converter::class)
@@ -34,6 +34,12 @@ abstract class ChatDatabase : RoomDatabase() {
             }
         }
 
+        private val migration_2_3 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE ChatRoomEntity ADD COLUMN quesetionImage TEXT DEFAULT ''")
+            }
+        }
+
         @Synchronized
         fun getInstance(context: Context): ChatDatabase? {
             if (instance == null) {
@@ -41,7 +47,7 @@ abstract class ChatDatabase : RoomDatabase() {
                     instance = Room.databaseBuilder(
                         context.applicationContext,
                         ChatDatabase::class.java, "chat.db"
-                    ).addMigrations(migration_1_2)
+                    ).addMigrations(migration_1_2).addMigrations(migration_2_3)
                         .build()
                 }
             }
